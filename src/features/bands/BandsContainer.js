d381eebba88b89a8b58d11fb9c041f6398934510
Alessandra Chardon <achardon@ucla.edu>
2022-05-05 09:17:10 -0400

    commit tree 1b35c919e95c3e971a60ef3361b4da43f1bd41fa

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,29 @@
 import React from "react";
+import BandInput from "./BandInput";
+import { useSelector, useDispatch } from "react-redux";
+import { bandAdded } from "./bandsSlice";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+
+  const bands = useSelector(state => state.bands.entities)
+  // console.log(bands)
+
+  const dispatch = useDispatch()
+  // console.log(dispatch)
+
+  function onBandSubmit(name) {
+    dispatch(bandAdded(name))
+  }
+
+  return (
+    <div>
+      <h1>Bands Container</h1>
+      <BandInput onBandSubmit={onBandSubmit} />
+      <div>
+        {bands.map(band => <li key={band}>{band}</li>)}
+      </div>
+    </div>
+  );
 }
 
 export default BandsContainer;
