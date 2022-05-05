00a96114e9dc6d2de5df5d658869d3d88a0b24da
Alessandra Chardon <achardon@ucla.edu>
2022-05-05 09:17:19 -0400

    assignment complete

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput( {onBandSubmit} ) {
+  const [name, setName] = useState("")
+
+  function handleChange(e) {
+    setName(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onBandSubmit(name)
+    setName("")
+  }
+
+  return (
+      <form onSubmit={handleSubmit}>
+        <label>Name: 
+          <input label="name" type="text" value={name} onChange={handleChange} />
+        </label>
+        <input type="submit" value="add band"/>
+      </form>
+  )
 }
 
 export default BandInput;
